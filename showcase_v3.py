3f6abf9a402bdb2379170eba190896889c055e7d
agmillsap <agmillsap@outlook.com>
2026-08-15 10:37:23 -0400

    Bump Showcase component for final visual cleanup

--- showcase_v3.py
+++ showcase_v3.py
@@ -191,7 +191,7 @@ def _payload() -> dict[str, object]:
 
 
 SHOWCASE_COMPONENT = st.components.v2.component(
-    name="abiq_public_showcase_v8",
+    name="abiq_public_showcase_v9",
     html=_read_text(FRONTEND / "showcase_exec.html"),
     css=(
         _read_text(FRONTEND / "showcase.css")
@@ -241,7 +241,7 @@ def run() -> None:
     SHOWCASE_COMPONENT(
         data=_payload(),
         default={},
-        key="abiq_public_showcase_v8",
+        key="abiq_public_showcase_v9",
         width="stretch",
         height="content",
     )
